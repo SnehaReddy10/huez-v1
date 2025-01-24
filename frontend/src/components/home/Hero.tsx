dd01c97935f935b3260294fa3f0d3f171581ac17
Sneha Reddy <snehareddy9178@gmail.com>
2025-01-24 22:57:51 +0530

    add hero

---
+++ frontend/src/components/home/Hero.tsx
@@ -0,0 +1,30 @@
+import PrimaryInput from '../inputs/PrimaryInput';
+import Typewriter from '../common/Typewriter';
+
+function Hero() {
+  return (
+    <div className="">
+      <img
+        src="https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+        className="w-screen background"
+      />
+      <div className="absolute inset-0 text-white font-serif mt-20 md:mt-40">
+        <div className="h-screen flex flex-col gap-4 items-center">
+          <p className="text-xs md:text-xl w-48 md:w-96 text-center font-bold">
+            <Typewriter
+              text="Bringing restaurant-quality meals to your table without compromise!"
+              delay={60}
+            />
+          </p>
+          <PrimaryInput
+            placeholder="Search for item or restaurant"
+            className="absolute mt-20 rounded-full transition-all ease-in md:px-5 md:py-2 h-min focus:outline-none text-black-900"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
