c03b6a08bf2196471c195ec0807606e71e9c3aba
Sneha Reddy <snehareddy9178@gmail.com>
2025-01-24 22:57:15 +0530

    add navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,100 @@
+import { IoIosRestaurant, IoMdInformationCircle } from 'react-icons/io';
+import { IoHome } from 'react-icons/io5';
+import { MdLocalOffer, MdMenuBook } from 'react-icons/md';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const navItems = [
+  {
+    id: 1,
+    label: 'Home',
+    path: '/',
+    icon: <IoHome color="white" size={15} fill="white" />,
+  },
+  {
+    id: 2,
+    label: 'Restaurants',
+    path: '/restaurants',
+    icon: <IoIosRestaurant color="white" size={20} fill="white" />,
+  },
+  {
+    id: 3,
+    label: 'Menu',
+    path: '/menu',
+    icon: <MdMenuBook color="white" size={20} fill="white" />,
+  },
+  {
+    id: 4,
+    label: 'Offers',
+    path: '/offers',
+    icon: <MdLocalOffer color="white" size={15} fill="white" />,
+  },
+  {
+    id: 5,
+    label: 'About',
+    path: '/about',
+    icon: <IoMdInformationCircle color="white" size={15} fill="white" />,
+  },
+];
+
+const authItems = [
+  {
+    id: 1,
+    label: 'Register',
+    path: '/register',
+  },
+  {
+    id: 2,
+    label: 'login',
+    path: '/login',
+  },
+];
+
+function Navbar() {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleOnClick = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <div className="z-10 flex gap-4 sticky top-0 justify-around text-xxs py-3 bg-black-900 text-white">
+      <div className="flex gap-8 justify-center">
+        <span className="pr-10 text-sm font-bold text-orange-500 selection:text-white">
+          Huez
+        </span>
+        {navItems.map((x) => (
+          <div
+            key={x.id}
+            onClick={() => handleOnClick(x.path)}
+            className="flex flex-col gap-[0.12rem] cursor-pointer"
+          >
+            <div className="flex gap-1 items-center">
+              {x.icon}
+              <span>{x.label}</span>
+            </div>
+            {x.path === pathname && (
+              <span className="bg-orange-500 h-[0.12rem] rounded-full w-6"></span>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-4 justify-center">
+        {authItems.map((x) => (
+          <div
+            key={x.id}
+            onClick={() => handleOnClick(x.path)}
+            className="flex flex-col gap-[0.12rem] cursor-pointer"
+          >
+            <span>{x.label}</span>
+            {x.path === pathname && (
+              <span className="bg-orange-500 h-[0.12rem] rounded-full w-3"></span>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
