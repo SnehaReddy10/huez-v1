a89f8f9439cf12f6753ec0fcc8e0cf6c5eebba88
Sneha Reddy <snehareddy9178@gmail.com>
2025-01-30 22:59:15 +0530

    add all routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
+import Login from './components/auth/Login';
+import Navbar from './components/Navbar';
+import Restaurants from './components/Restaurants';
+import Register from './components/auth/Register';
+import Product from './components/Product';
+import Menu from './components/Menu';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <div className="flex flex-col selection:bg-orange-500 h-screen max-w-screen">
+      <div className="hidden md:flex flex-col">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
