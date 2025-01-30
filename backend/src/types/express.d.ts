613756f3c99126f580dde8eb715b1b0f216ec760
Sneha Reddy <snehareddy9178@gmail.com>
2025-01-30 23:02:20 +0530

    feat: add login page

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,4 +1,4 @@
-import { User } from '../models/user.model'; // Adjust path to your User model
+import { User } from 'src/models/User.model';
 
 declare global {
   namespace Express {
@@ -9,13 +9,13 @@ declare global {
       email: string;
       role: string;
       authProvider: string;
-      restaurants: string[]; // Adjust as needed
+      restaurants: string[];
       createdAt: Date;
       __v?: number;
     }
 
     interface Request {
-      user?: User; // Add user to Request
+      user?: User;
     }
   }
 }
---
+++ frontend/src/components/auth/Login.tsx
@@ -0,0 +1,112 @@
+import { useNavigate } from 'react-router-dom';
+import PrimaryButton from '../buttons/PrimaryButton';
+import TertiaryInput from '../inputs/TertiaryInput';
+import { useLoginMutation } from '../../store';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useContext, useEffect } from 'react';
+import { ToastContext } from '../../context/ToastContext';
+import { loginSchema } from '../../validators/LoginSchema';
+
+function Login() {
+  const [login, results] = useLoginMutation();
+  const toastContext = useContext(ToastContext);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid, errors },
+  } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(loginSchema),
+  });
+
+  useEffect(() => {
+    if (results.error) {
+      const r = results.error as any;
+      if (!toastContext) {
+        throw new Error('useContext must be used within a ToastProvider');
+      }
+
+      const { showToast } = toastContext;
+      showToast(r.data.message ?? r.data.error[0], 'error', 'right-0 top-10');
+      reset();
+    }
+  }, [results.error, reset]);
+
+  const navigate = useNavigate();
+
+  if (results.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (results.isSuccess) {
+    navigate('/');
+  }
+
+  const handleLogin = (data: any) => {
+    console.log({ data });
+    login(data);
+  };
+
+  return (
+    <div className="h-full flex flex-col md:flex-row justify-between items-center">
+      <div className="animate-slideBottom md:animate-slideRight login h-[70%] w-full md:w-[70%] flex items-center justify-center md:h-full p-4 md:p-10">
+        <div className="flex flex-col animate-fadeIn gap-4 md:gap-10 items-center justify-center w-max md:w-72">
+          <h2 className="text-xl md:text-3xl font-semibold font-serif">
+            Welcome back,
+          </h2>
+          <TertiaryInput
+            innerRef={register('email')}
+            error={errors.email?.message}
+            placeholder="john.dow@gmail.com"
+            label="email"
+          />
+          <TertiaryInput
+            innerRef={register('password')}
+            error={errors.password?.message}
+            placeholder="secret"
+            label="password"
+            type="password"
+          />
+          <div className="flex flex-col gap-2">
+            <PrimaryButton
+              disabled={!isValid}
+              onClickHandler={handleSubmit(handleLogin)}
+              label="SIGN IN"
+              className="rounded-full w-56 md:w-72 md:text-xs py-2 font-semibold"
+            />
+            <PrimaryButton
+              onClickHandler={() => {}}
+              label="Connect with Google"
+              className="rounded-full w-56 md:w-72 md:text-xs py-2 font-semibold bg-white border-black-900 border-2 text-black-900"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="register animate-slideTop md:animate-slideLeft h-[30%] w-full md:w-[30%] flex flex-col md:h-full justify-center text-center p-4 md:p-10 bg-cover text-white bg-[url('https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=600')]">
+        <div className="flex flex-col animate-fadeIn gap-2 md:gap-40">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-xl md:text-3xl font-semibold font-serif">
+              New here?
+            </h2>
+            <p className="text-gray-300 text-xxs md:text-xs text-balance">
+              Sign up now and explore a world of delicious meals at your
+              doorstep!
+            </p>
+          </div>
+          <div className="pb-4 md:pb-10">
+            <PrimaryButton
+              onClickHandler={() => navigate('/register')}
+              label="SIGN UP"
+              className="rounded-full md:text-xs py-1 md:py-2 px-4 w-max font-semibold bg-white hover:bg-transparent hover:text-white transition-all ease-in border-white border-2 text-black-900"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
