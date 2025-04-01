84f76a3ce46547ad44a62ebfde779d55bb71c2b7
Sneha Reddy <snehareddy9178@gmail.com>
2025-04-01 22:12:04 +0530

    capture value in primary input

--- frontend/src/components/inputs/PrimaryInput.tsx
+++ frontend/src/components/inputs/PrimaryInput.tsx
@@ -1,19 +1,26 @@
+import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
 function PrimaryInput({
   placeholder,
+  value,
   className = '',
+  onChange,
 }: {
   placeholder: string;
+  value: string;
   className?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
   return (
     <input
       type="text"
+      value={value}
       className={twMerge(
         `border-[1px] p-1 rounded-sm border-[#A59D84] text-xs w-60 ${className}`
       )}
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 }
